c639e4e2613f8aa738c0155225f8ab926fdfd662
Binh2408 <nvbinh24082002@gmail.com>
2025-07-31 15:16:16 +0700

    update

--- CALL_API/call_api_json_axiois/src/service/categoryService.js
+++ CALL_API/call_api_json_axiois/src/service/categoryService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 export async function findAllCategory() {
   try {
-    const response = await axios.get("http://localhost:8080/categories");
+    // const response = await axios.get("http://localhost:8080/categories");
+    const response = await axios.get("https://json-server-api-test-2.onrender.com/categories");
     return response.data;
   } catch (error) {
     console.log(error);
